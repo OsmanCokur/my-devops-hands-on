72a2f840ef75fbbb2d1a3f2996a9a93aca5a8ec5
Osman ÇOKUR <smnckr2107.gmail.com>
2022-06-24 10:31:57 +0300

    update

--- tf-com-var-cond-loops/main.tf
+++ tf-com-var-cond-loops/main.tf
@@ -11,23 +11,24 @@ terraform {
   }
 }
 
+locals {
+  mytag = "smn-local-name"
+}
+
 resource "aws_instance" "tf-ec2" {
-  ami           = "ami-0ed9277fb7eb570c9"
-  instance_type = "t2.micro"
+  ami           = var.ec2_ami
+  instance_type = var.ec2_type
   key_name      = "firstkey"    # write your pem file without .pem extension>
   tags = {
-    "Name" = "tf-ec2"
+    Name = "${local.mytag}-come from locals"
   }
 }
 
+
 resource "aws_s3_bucket" "tf-s3" {
   bucket = "smnckr-tf-test-bucket-addwhateveryouwant"
+  tags = {
+    Name = "${local.mytag}-come from locals"
+  }
 }
 
-output "tf_example_public_ip" {
-  value = aws_instance.tf-ec2.public_ip
-}
-
-output "tf_example_s3_meta" {
-  value = aws_s3_bucket.tf-s3.region
-}
